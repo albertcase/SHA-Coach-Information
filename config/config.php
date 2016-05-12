ee5f44795d0a0642cb91a6b0ba880951bf852d9c
Demon Zhang <demon.zhang@samesamechina.com>
2016-05-12 14:49:14 +0800

    no message

--- config/config.php
+++ config/config.php
@@ -4,7 +4,7 @@
 define("REDIS_HOST", '127.0.0.1');
 define("REDIS_PORT", '6379');
 
-define("BASE_URL", 'http://coachvip.samesamechina.com/');
+define("BASE_URL", 'http://coach-information.samesamechina.com/');
 define("TEMPLATE_ROOT", dirname(__FILE__) . '/../template');
 
 //当前时间
@@ -14,7 +14,7 @@ define("WX_APPID", 'wx737a6d5fe4d19c89');
 //微信商户id
 define("WX_BUSID", '1275055201');
 //Curio接口
-define("CURIO_AUTH_URL", 'http://coach.samesamechina.com/api/wechat/oauth/auth/9b2cea78-4b43-4c04-8f61-b8ebd1601e3b');
+define("CURIO_AUTH_URL", 'http://coach.samesamechina.com/api/wechat/oauth/auth/6d9002e0-e097-468b-a78f-8fe834f5ab96');
 define("CURIO_TOKEN", 'zcBpBLWyAFy6xs3e7HeMPL9zWrd7Xy');
 define("CURIO_JS_ID", '912e9ed6-7426-49c0-98f2-903fa4bf0d7a');
 
@@ -22,7 +22,7 @@ define("CURIO_JS_ID", '912e9ed6-7426-49c0-98f2-903fa4bf0d7a');
 define("DBHOST", '127.0.0.1');
 define("DBUSER", 'root');
 define("DBPASS", '1qazxsw2');
-define("DBNAME", 'coach_vip');
+define("DBNAME", 'coach_information');
 
 
 
