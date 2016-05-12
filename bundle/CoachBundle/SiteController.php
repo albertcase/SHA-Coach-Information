a436855737f548ba5a4386d93650da170c15ba5d
Demon Zhang <demon.zhang@samesamechina.com>
2016-05-12 14:52:31 +0800

    no message

--- bundle/CoachBundle/SiteController.php
+++ bundle/CoachBundle/SiteController.php
@@ -7,20 +7,18 @@ use Core\Controller;
 class SiteController extends Controller {
 
 	public function indexAction() {	
-		// $UserAPI = new \Lib\UserAPI();
-		// $user = $UserAPI->userLoad(true);
-		// if (!$user) {
-		// 	$parameterAry = $_GET;
-		// 	if(count($parameterAry)>0)
-		// 		$url = "/?".http_build_query($parameterAry);
-		// 	else
-		// 		$url = "/";
-		// 	$_SESSION['redirect_url'] = $url;
-		// 	$WechatAPI = new \Lib\WechatAPI();
-		// 	$WechatAPI->wechatAuthorize();
-		// }
-		// $databaseapi = new \Lib\DatabaseAPI();
-		// $type = $databaseapi->checkuser($user->openid);
+		$UserAPI = new \Lib\UserAPI();
+		$user = $UserAPI->userLoad(true);
+		if (!$user) {
+			$parameterAry = $_GET;
+			if(count($parameterAry)>0)
+				$url = "/?".http_build_query($parameterAry);
+			else
+				$url = "/";
+			$_SESSION['redirect_url'] = $url;
+			$WechatAPI = new \Lib\WechatAPI();
+			$WechatAPI->wechatAuthorize();
+		}
 		$this->render('site/index');
 		exit;
 	}
