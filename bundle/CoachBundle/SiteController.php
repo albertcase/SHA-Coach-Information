345f804532bb3f9c41dbfe4e9fe8f1de4e5d826e
ke.wang@samesamechina.com <ke.wang@samesamechina.com>
2017-06-09 17:10:12 +0800

    fixed:coachinformation

--- bundle/CoachBundle/SiteController.php
+++ bundle/CoachBundle/SiteController.php
@@ -6,32 +6,36 @@ use Core\Controller;
 
 class SiteController extends Controller {
 
-	public function indexAction($card) {
-		$UserAPI = new \Lib\UserAPI();
-		$user = $UserAPI->userLoad(true);
-		if (!$user) {
-			$parameterAry = $_SERVER['REQUEST_URI'];
-			if($parameterAry != '')
-				$url = "/".$parameterAry;
-			else
-				$url = "/";
-			$_SESSION['redirect_url'] = $url;
-			$WechatAPI = new \Lib\WechatAPI();
-			$WechatAPI->wechatAuthorize();
-		}
-
-        if(empty($card)){
-            $this->render('site/index');
-        }else {
-            $cardList = json_decode(CARD_LIST, 1);
-            if(!array_key_exists($card, $cardList)) {
-                die('cardid not known');
-            }
-            $wechatapi = new \Lib\WechatAPI();
-            $list = $wechatapi->cardList($cardList[$card]);
-
-            $this->render('site/card', array('list'=>$list));
+    public function indexAction() {
+        $UserAPI = new \Lib\UserAPI();
+        $user = $UserAPI->userLoad(true);
+        if (!$user) {
+            $parameterAry = $_GET;
+            if(count($parameterAry)>0)
+                $url = "/?".http_build_query($parameterAry);
+            else
+                $url = "/";
+            $_SESSION['redirect_url'] = $url;
+            $WechatAPI = new \Lib\WechatAPI();
+            $WechatAPI->wechatAuthorize();
         }
+
+        $this->render('site/index');
         exit;
+    }
+
+//        if(empty($card)){
+//            $this->render('site/index');
+//        }else {
+//            $cardList = json_decode(CARD_LIST, 1);
+//            if(!array_key_exists($card, $cardList)) {
+//                die('cardid not known');
+//            }
+//            $wechatapi = new \Lib\WechatAPI();
+//            $list = $wechatapi->cardList($cardList[$card]);
+//
+//            $this->render('site/card', array('list'=>$list));
+//        }
+//        exit;
     }
 }
